72f9b466c04b3aa9fb6031e326f9a5b9334dde50
tatiana <tatiana.perry@bigcommerce.com>
2019-06-28 08:54:15 -0500

    added prod file

--- app/components/Checkout.js
+++ app/components/Checkout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { createCheckoutService } from '@bigcommerce/checkout-sdk';
 import { embedCheckout } from '@bigcommerce/checkout-sdk';
 const service = createCheckoutService();
@@ -7,9 +7,9 @@ class Checkout extends React.Component {
     render(){
         embedCheckout({
             "url": "https://tatiana.fun/cart.php?embedded=1&action=loadInCheckout&id=b15bd57c-1b3c-4862-ae5a-2ddb35910a75&token=c617572754f0b35262c1fb67eefa5148d9379a6a934d05b291a0b43db61f4306",
-            "containerId": "app"
+            "containerId": "bc-checkout-container"
         })
-    return embedCheckout
+    return(embedCheckout)
     } 
 }
     
